49a18a79304d59ef193fa68cfdf79d7227b85709
Dibyendu Pramanik <chotuu668@gmil.com>
2024-12-23 21:09:01 +0530

    update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -134,7 +134,7 @@ const ProductCard = ({ product, isInWishlist, setLatesData}) => {
           ) : (
             <button
               onClick={handleWishlist}
-              className="w-full py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors duration-200"
+              className="btn btn-warning"
             >
               Add to Wishlist
             </button>
